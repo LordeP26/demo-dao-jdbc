7ce313c6b95ed6ed150158226fb775f7e326687f
LordeP26 <ped.li2608@gmail.com>
2025-01-16 14:33:29 -0300

    Creating project and git repository

---
+++ src/applicaiton/Program.java
@@ -0,0 +1,16 @@
+package applicaiton;
+
+public class Program {
+	
+	/*  DAO: para cada entidade, haverá um objeto responsável por fazer acesso a dados relacionado a esta entidade  
+	 * ex: Cliente = ClienteDao/ Produto = ProdutoDao/ Pedido = PedidoDao
+	 * cada DAO será definido por uma interface
+	 * a injeção de dependência pode ser feita por meio do padrão de projeto Factory
+	 */
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
