89740a47b1b23f4009c288b5067bab513bf3fa02
LordeP26 <ped.li2608@gmail.com>
2025-01-17 15:37:57 -0300

    Insert Implementation

--- src/applicaiton/Program.java
+++ src/applicaiton/Program.java
@@ -1,5 +1,6 @@
 package applicaiton;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -36,7 +37,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n === TESTE 4: seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 } 
+
 
