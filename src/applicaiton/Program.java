6cf48e2f9f7477a118088147f0c307219e06bdb7
LordeP26 <ped.li2608@gmail.com>
2025-01-17 16:12:26 -0300

    Delete Implementation

--- src/applicaiton/Program.java
+++ src/applicaiton/Program.java
@@ -1,7 +1,9 @@
 package applicaiton;
 
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -16,7 +18,11 @@ public class Program {
 	 * a injeção de dependência pode ser feita por meio do padrão de projeto Factory
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException{
+		
+		Scanner sc = new Scanner(System.in);	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -26,7 +32,7 @@ public class Program {
 		System.out.println(seller);
 		
 		System.out.println("\n === TESTE 2: seller findByDepartment ===");
-		Department department = new Department(2, null);
+		Department department = new Department(4, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
@@ -39,15 +45,24 @@ public class Program {
 		}
 		
 		System.out.println("\n === TESTE 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		Seller newSeller = new Seller(null, "Monique ", "monique22022005@gmail.com", sdf.parse("22/02/2005"), 2400.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 		
 		System.out.println("\n === TESTE 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n === TESTE 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 } 
